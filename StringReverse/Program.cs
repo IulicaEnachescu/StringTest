9aaa91d6e97ba1637a4c77e02a045668f75ba571
IulicaEnachescu <iulica.enachescu@gmail.com>
2020-01-07 20:22:26 +0200

    StringTest

---
+++ StringReverse/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                String str = "ab-cd";
+                string revStr = ReverseString(str);
+                Console.WriteLine("Output string: " + revStr);
+                str = "aa-bC-dEf-ghIj";
+                revStr = ReverseString(str);
+                Console.WriteLine("Output string: " + revStr);
+                str = "Test1ng-Leet=code-Q";
+                revStr = ReverseString(str);
+                Console.WriteLine("Output string: " + revStr);
+                str = "";
+                revStr = ReverseString(str);
+                Console.WriteLine("Output string: " + revStr);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+               // Console.WriteLine(e.StackTrace);
+            }
+            Console.ReadLine();
+
+        }
+
+        public static string ReverseString(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentException("Exception: Empty string: ");
+            }
+            char[] charArray = str.ToCharArray();
+
+            Console.WriteLine("Input string: " + str);
+            reverse(charArray);
+            String revStr = new String(charArray);
+            return revStr;
+        }
+
+        public static void reverse(char[] str)
+        {   
+            int r = str.Length - 1;
+            int l = 0;
+                          
+            while (l < r)
+            { 
+                if (!char.IsLetter(str[l]))
+                    l++;
+                else if (!char.IsLetter(str[r]))
+                    r--;  
+                else
+                {
+                    Swap(ref str[r], ref str[l]);
+                    l++;
+                    r--;
+                }
+            }
+        }
+       
+        static void Swap<T>(ref T left, ref T right )
+        {
+            T tmp;
+            tmp= left;
+            left = right;
+            right = tmp;
+        }
+    }
+}
---
+++ StringReverseTest/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringReverse;
+using NUnit.Framework;
+
+namespace StringReverseTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        
+        [Test]
+        [TestCase("ab-cd", "dc-ba")]
+        [TestCase("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [TestCase("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+        public void Should_Reverse_A_Valid_String(string input, string expected)
+        {
+            //arrange
+            //string input = "ab-cd";
+            //string expected = "dc-ba";
+
+            //act
+            string actual = Program.ReverseString(input);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [Test]
+        public void Should_Throw_Exception_When_Empty()
+        {//arrange
+            string input = null;
+            //act
+
+            //assert
+            Assert.Throws<ArgumentException>(() => Program.ReverseString(input));
+
+        }
+       
+
+    }
+}
